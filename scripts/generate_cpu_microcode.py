1ed4d71321bd929cf0a8c8cc4c972cd8d3ffcff2
Leonardo Nicolas <leonicolas@gmail.com>
2019-07-03 23:51:16 -0300

    Add add, sub and out instructions.

--- scripts/generate_cpu_microcode.py
+++ scripts/generate_cpu_microcode.py
@@ -15,33 +15,65 @@ fin_inst = [0x0010]
 # cf = carry flag.
 # zf = zero flag.
 instruction_set = [
-    {   'name': 'halt', 
-        'op_code': 0x0, 'af': [0, 1], 'cf': [0, 1], 'zf': [0, 1],
+    {   'name': 'halt',
+        'op_code': 0x00, 'cf': [0, 1], 'zf': [0, 1],
         'flags': fetch + [ 0x8000 ] + fin_inst
     },
     {   'name': 'lda_num',
-        'op_code': 0x1, 'af': 0, 'cf': [0, 1], 'zf': [0, 1],
+        'op_code': 0x01, 'cf': [0, 1], 'zf': [0, 1],
         'flags': fetch + [ 0x010C, 0x4040 ] + fin_inst
     },
     {   'name': 'lda_addr',
-        'op_code': 0x1, 'af': 1, 'cf': [0, 1], 'zf': [0, 1],
+        'op_code': 0x02, 'cf': [0, 1], 'zf': [0, 1],
         'flags': fetch + [ 0x010C, 0x0140, 0x4040 ] + fin_inst
     },
     {   'name': 'sta_addr',
-        'op_code': 0x2, 'af': [0, 1], 'cf': [0, 1], 'zf': [0, 1],
+        'op_code': 0x03, 'cf': [0, 1], 'zf': [0, 1],
         'flags': fetch + [ 0x010C, 0x0140, 0x2080 ] + fin_inst
     },
     {   'name': 'ldb_num',
-        'op_code': 0x3, 'af': 0, 'cf': [0, 1], 'zf': [0, 1],
+        'op_code': 0x04, 'cf': [0, 1], 'zf': [0, 1],
         'flags': fetch + [ 0x010C, 0x0440 ] + fin_inst
     },
     {   'name': 'ldb_addr',
-        'op_code': 0x3, 'af': 1, 'cf': [0, 1], 'zf': [0, 1],
+        'op_code': 0x05, 'cf': [0, 1], 'zf': [0, 1],
         'flags': fetch + [ 0x010C, 0x0140, 0x0440 ] + fin_inst
     },
     {   'name': 'stb_addr',
-        'op_code': 0x4, 'af': [0, 1], 'cf': [0, 1], 'zf': [0, 1],
+        'op_code': 0x06, 'cf': [0, 1], 'zf': [0, 1],
         'flags': fetch + [ 0x010C, 0x0140, 0x0280 ] + fin_inst
+    },
+    {   'name': 'add_num',
+        'op_code': 0x07, 'cf': [0, 1], 'zf': [0, 1],
+        'flags': fetch + [ 0x010C, 0x0440, 0x5000 ] + fin_inst
+    },
+    {   'name': 'add_addr',
+        'op_code': 0x08, 'cf': [0, 1], 'zf': [0, 1],
+        'flags': fetch + [ 0x010C, 0x0140, 0x0440, 0x5000 ] + fin_inst
+    },
+    {   'name': 'sub_num',
+        'op_code': 0x09, 'cf': [0, 1], 'zf': [0, 1],
+        'flags': fetch + [ 0x010C, 0x0C40, 0x5800 ] + fin_inst
+    },
+    {   'name': 'sub_addr',
+        'op_code': 0x0A, 'cf': [0, 1], 'zf': [0, 1],
+        'flags': fetch + [ 0x010C, 0x0140, 0x0C40, 0x5800 ] + fin_inst
+    },
+    {   'name': 'outa',
+        'op_code': 0x0B, 'cf': [0, 1], 'zf': [0, 1],
+        'flags': fetch + [ 0x2001 ] + fin_inst
+    },
+    {   'name': 'outb',
+        'op_code': 0x0C, 'cf': [0, 1], 'zf': [0, 1],
+        'flags': fetch + [ 0x0201 ] + fin_inst
+    },
+    {   'name': 'out_num',
+        'op_code': 0x0D, 'cf': [0, 1], 'zf': [0, 1],
+        'flags': fetch + [ 0x010C, 0x0041 ] + fin_inst
+    },
+    {   'name': 'out_addr',
+        'op_code': 0x0E, 'cf': [0, 1], 'zf': [0, 1],
+        'flags': fetch + [ 0x010C, 0x0140, 0x0041 ] + fin_inst
     },
 ]
 
@@ -52,35 +84,33 @@ def cast_array(value):
 # Print the microcode
 def print_microcode(microcode):
     for instruction in microcode:
-        op_code = instruction['address'] >> 6 
-        af = (instruction['address'] & 0x20) >> 5
+        op_code = instruction['address'] >> 5
         cf = (instruction['address'] & 0x10) >> 4
         zf = (instruction['address'] & 0x08) >> 3
         step = instruction['address'] & 0x07
-        print("{} - 0x{:04x} - {:04b} {:01b} {:01b} {:01b} {:03b} - {:04x}".format(
+        print("{} - 0x{:04x} - {:05b} {:01b} {:01b} {:03b} - {:04x}".format(
             instruction['name'].ljust(10),
             instruction['address'],
-            op_code, af, cf, zf, step, 
-            instruction['flag'])) 
+            op_code, cf, zf, step,
+            instruction['flag']))
 
 # Transforms an array of arrays in an single array.
 def flat_array(data):
     return [value for internal_data in data for value in internal_data]
 
 
-def create_instruction_microcode(instruction, af, cf, zf):
+def create_instruction_microcode(instruction, cf, zf):
     step = 0
     instruction_steps = list()
     # Loop over flags
     for flag in cast_array(instruction['flags']):
         instruction_microcode = {
             'name': instruction['name'],
-            'address': instruction['op_code'] << 6 
-                | af << 5 
-                | cf << 4 
-                | zf << 3 
+            'address': instruction['op_code'] << 5
+                | cf << 4
+                | zf << 3
                 | step,
-            'flag': flag 
+            'flag': flag
         }
         instruction_steps.append(instruction_microcode)
         step += 1
@@ -93,9 +123,7 @@ def generate_microcode(fetch, instruction_set):
 
     # Loop over the instructions
     for instruction in instruction_set:
-        instruction_steps = [ create_instruction_microcode(instruction, af, cf, zf)
-            # Loop over the address flag
-            for af in cast_array(instruction['af'])
+        instruction_steps = [ create_instruction_microcode(instruction, cf, zf)
             # Loop over the carry flag
             for cf in cast_array(instruction['cf'])
             # Loop over the zero flag
@@ -103,7 +131,7 @@ def generate_microcode(fetch, instruction_set):
         ]
 
         microcode += flat_array(instruction_steps)
-    
+
     return microcode
 
 # Fills the microcode missing addresses with NOP (no operation)
@@ -145,7 +173,7 @@ if verbose:
 # Saves the code table in a ROM file.
 instruction_size = 16 # bytes
 save_rom.save_file(
-    file_name, 
-    [instruction_step["flag"] for instruction_step in microcode], 
+    file_name,
+    [instruction_step["flag"] for instruction_step in microcode],
     instruction_size)
 
