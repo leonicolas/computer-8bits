e3be3a741b419b3767637cdfc6b2c80ef6be5752
Leonardo Nicolas <leonicolas@gmail.com>
2019-07-06 20:35:59 -0300

    Add flags register and improve microcode generation.

--- scripts/generate_cpu_microcode.py
+++ scripts/generate_cpu_microcode.py
@@ -21,103 +21,83 @@ instruction_set = [
     },
     {   'name': 'halt',
         'op_code': 0x01, 'cf': [0, 1], 'zf': [0, 1],
-        'flags': fetch + [ 0x8000 ] + fin_inst
+        'flags': fetch + [ 0x08000 ] + fin_inst
     },
     {   'name': 'lda_num',
         'op_code': 0x02, 'cf': [0, 1], 'zf': [0, 1],
-        'flags': fetch + [ 0x010C, 0x4040 ] + fin_inst
+        'flags': fetch + [ 0x0010C, 0x04040 ] + fin_inst
     },
-    {   'name': 'lda_paddr',
+    {   'name': 'lda_addr',
         'op_code': 0x03, 'cf': [0, 1], 'zf': [0, 1],
-        'flags': fetch + [ 0x010C, 0x0140, 0x4040 ] + fin_inst
+        'flags': fetch + [ 0x0010C, 0x00140, 0x04040 ] + fin_inst
     },
-    {   'name': 'sta_paddr',
+    {   'name': 'sta_addr',
         'op_code': 0x04, 'cf': [0, 1], 'zf': [0, 1],
-        'flags': fetch + [ 0x010C, 0x0140, 0x2080 ] + fin_inst
+        'flags': fetch + [ 0x0010C, 0x00140, 0x02080 ] + fin_inst
     },
     {   'name': 'ldb_num',
         'op_code': 0x05, 'cf': [0, 1], 'zf': [0, 1],
-        'flags': fetch + [ 0x010C, 0x0440 ] + fin_inst
+        'flags': fetch + [ 0x0010C, 0x00440 ] + fin_inst
     },
-    {   'name': 'ldb_paddr',
+    {   'name': 'ldb_addr',
         'op_code': 0x06, 'cf': [0, 1], 'zf': [0, 1],
-        'flags': fetch + [ 0x010C, 0x0140, 0x0440 ] + fin_inst
+        'flags': fetch + [ 0x0010C, 0x00140, 0x00440 ] + fin_inst
     },
-    {   'name': 'stb_paddr',
+    {   'name': 'stb_addr',
         'op_code': 0x07, 'cf': [0, 1], 'zf': [0, 1],
-        'flags': fetch + [ 0x010C, 0x0140, 0x0280 ] + fin_inst
+        'flags': fetch + [ 0x0010C, 0x00140, 0x00280 ] + fin_inst
     },
     {   'name': 'add_num',
         'op_code': 0x08, 'cf': [0, 1], 'zf': [0, 1],
-        'flags': fetch + [ 0x010C, 0x0440, 0x5000 ] + fin_inst
+        'flags': fetch + [ 0x0010C, 0x00440, 0x15000 ] + fin_inst
     },
-    {   'name': 'add_paddr',
+    {   'name': 'add_addr',
         'op_code': 0x09, 'cf': [0, 1], 'zf': [0, 1],
-        'flags': fetch + [ 0x010C, 0x0140, 0x0440, 0x5000 ] + fin_inst
+        'flags': fetch + [ 0x0010C, 0x00140, 0x00440, 0x15000 ] + fin_inst
     },
     {   'name': 'sub_num',
         'op_code': 0x0A, 'cf': [0, 1], 'zf': [0, 1],
-        'flags': fetch + [ 0x010C, 0x0C40, 0x5800 ] + fin_inst
+        'flags': fetch + [ 0x0010C, 0x00C40, 0x15800 ] + fin_inst
     },
-    {   'name': 'sub_paddr',
+    {   'name': 'sub_addr',
         'op_code': 0x0B, 'cf': [0, 1], 'zf': [0, 1],
-        'flags': fetch + [ 0x010C, 0x0140, 0x0C40, 0x5800 ] + fin_inst
+        'flags': fetch + [ 0x0010C, 0x00140, 0x00C40, 0x15800 ] + fin_inst
     },
     {   'name': 'outa',
         'op_code': 0x0C, 'cf': [0, 1], 'zf': [0, 1],
-        'flags': fetch + [ 0x2001 ] + fin_inst
+        'flags': fetch + [ 0x02001 ] + fin_inst
     },
     {   'name': 'outb',
         'op_code': 0x0D, 'cf': [0, 1], 'zf': [0, 1],
-        'flags': fetch + [ 0x0201 ] + fin_inst
+        'flags': fetch + [ 0x00201 ] + fin_inst
     },
     {   'name': 'out_num',
         'op_code': 0x0E, 'cf': [0, 1], 'zf': [0, 1],
-        'flags': fetch + [ 0x010C, 0x0041 ] + fin_inst
+        'flags': fetch + [ 0x0010C, 0x00041 ] + fin_inst
     },
-    {   'name': 'out_paddr',
+    {   'name': 'out_addr',
         'op_code': 0x0F, 'cf': [0, 1], 'zf': [0, 1],
-        'flags': fetch + [ 0x010C, 0x0140, 0x0041 ] + fin_inst
+        'flags': fetch + [ 0x0010C, 0x00140, 0x00041 ] + fin_inst
     },
     {   'name': 'jp_addr',
         'op_code': 0x10, 'cf': [0, 1], 'zf': [0, 1],
-        'flags': fetch + [ 0x010C, 0x0042 ] + fin_inst
-    },
-    {   'name': 'jp_paddr',
-        'op_code': 0x11, 'cf': [0, 1], 'zf': [0, 1],
-        'flags': fetch + [ 0x010C, 0x0140, 0x0042 ] + fin_inst
+        'flags': fetch + [ 0x0010C, 0x00042 ] + fin_inst
     },
     {   'name': 'jpz_addr_zf0',
-        'op_code': 0x12, 'cf': [0, 1], 'zf': 0,
-        'flags': fetch + fin_inst
+        'op_code': 0x11, 'cf': [0, 1], 'zf': 0,
+        'flags': fetch + [ 0x00008 ] + fin_inst
     },
     {   'name': 'jpz_addr_zf1',
-        'op_code': 0x12, 'cf': [0, 1], 'zf': 1,
-        'flags': fetch + [ 0x010C, 0x0042 ] + fin_inst
-    },
-    {   'name': 'jpz_paddr_zf0',
-        'op_code': 0x13, 'cf': [0, 1], 'zf': 0,
-        'flags': fetch + fin_inst
-    },
-    {   'name': 'jpz_paddr_zf1',
-        'op_code': 0x13, 'cf': [0, 1], 'zf': 1,
-        'flags': fetch + [ 0x010C, 0x0140, 0x0042 ] + fin_inst
+        'op_code': 0x11, 'cf': [0, 1], 'zf': 1,
+        'flags': fetch + [ 0x0010C, 0x00042 ] + fin_inst
     },
     {   'name': 'jpc_addr_cf0',
-        'op_code': 0x14, 'cf': 0, 'zf': [0, 1],
-        'flags': fetch + fin_inst
+        'op_code': 0x12, 'cf': 0, 'zf': [0, 1],
+        'flags': fetch + [ 0x00008 ] + fin_inst
     },
     {   'name': 'jpc_addr_cf1',
-        'op_code': 0x14, 'cf': 1, 'zf': [0, 1],
-        'flags': fetch + [ 0x010C, 0x0042 ] + fin_inst
-    },
-    {   'name': 'jpc_paddr_cf0',
-        'op_code': 0x15, 'cf': 0, 'zf': [0, 1],
-        'flags': fetch + fin_inst
-    },
-    {   'name': 'jpc_paddr_cf1',
-        'op_code': 0x15, 'cf': 1, 'zf': [0, 1],
-        'flags': fetch + [ 0x010C, 0x0140, 0x0042 ] + fin_inst
+        'op_code': 0x12, 'cf': 1, 'zf': [0, 1],
+        'flags': fetch + [ 0x0010C, 0x00042 ] + fin_inst
     },
 ]
 
@@ -132,7 +112,7 @@ def print_microcode(microcode):
         cf = (instruction['address'] & 0x10) >> 4
         zf = (instruction['address'] & 0x08) >> 3
         step = instruction['address'] & 0x07
-        print("{} - 0x{:04x} - {:05b} {:01b} {:01b} {:03b} - {:04x}".format(
+        print("{} - 0x{:04x} - {:05b} {:01b} {:01b} {:03b} - 0x{:05x}".format(
             instruction['name'].ljust(13),
             instruction['address'],
             op_code, cf, zf, step,
@@ -148,12 +128,10 @@ def create_instruction_microcode(instruction, cf, zf):
     instruction_steps = list()
     # Loop over flags
     for flag in cast_array(instruction['flags']):
+        address = instruction['op_code'] << 5 | cf << 4 | zf << 3 | step
         instruction_microcode = {
             'name': instruction['name'],
-            'address': instruction['op_code'] << 5
-                | cf << 4
-                | zf << 3
-                | step,
+            'address': address,
             'flag': flag
         }
         instruction_steps.append(instruction_microcode)
@@ -173,15 +151,18 @@ def generate_microcode(fetch, instruction_set):
             # Loop over the zero flag
             for zf in cast_array(instruction['zf'])
         ]
-
         microcode += flat_array(instruction_steps)
 
     return microcode
 
 # Fills the microcode missing addresses with NOP (no operation)
-def fill_microcode_paddresses(microcode):
+def fill_microcode_addresses(microcode):
     address = 0
     filled_microcode = list()
+    microcode = sorted(
+        microcode,
+        key = lambda item: item["address"],
+        reverse = False)
 
     # Loop over the microcode steps and complete the missing addresses.
     for instruction_step in microcode:
@@ -197,6 +178,9 @@ def fill_microcode_paddresses(microcode):
 
     return filled_microcode
 
+# sys.argv.append("-v")
+# sys.argv.append("test")
+
 # Validates the arguments.
 arguments_num = len(sys.argv)
 if(arguments_num <= 1 or arguments_num > 3):
@@ -210,12 +194,12 @@ file_name = sys.argv[2 if arguments_num == 3 else 1]
 
 # Generates the codes table.
 microcode = generate_microcode(fetch, instruction_set)
-microcode = fill_microcode_paddresses(microcode)
+microcode = fill_microcode_addresses(microcode)
 if verbose:
     print_microcode(microcode)
 
 # Saves the code table in a ROM file.
-instruction_size = 16 # bytes
+instruction_size = 20 # bytes
 save_rom.save_file(
     file_name,
     [instruction_step["flag"] for instruction_step in microcode],
